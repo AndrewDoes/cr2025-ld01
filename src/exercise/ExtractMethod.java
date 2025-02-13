35bc26067ed00aad6c0b51e63bc0a8254d6d1dce
Andrew Sutiono <andrewjogja1@hotmail.com>
2025-02-13 19:26:53 +0700

    extract all commented codes to separate methods

--- src/exercise/ExtractMethod.java
+++ src/exercise/ExtractMethod.java
@@ -22,20 +22,28 @@ public class ExtractMethod {
 	  Enumeration elements = orders.elements();
 	  double outstanding = 0.0;
 
-	  // print banner
-	  System.out.println ("*****************************");
-	  System.out.println ("****** Customer totals ******");
-	  System.out.println ("*****************************");
-
-	  // print owings
-	  while (elements.hasMoreElements()) {
-	    Order each = (Order) elements.nextElement();
-	    outstanding += each.getAmount();
-	  }
+	  printBanner();
+	  outstanding = printOwings(elements, outstanding);
 
 	  // print details
-	  System.out.println("name: " + name);
-	  System.out.println("amount: " + outstanding);
+	  printDetails(outstanding);
+	}
+	private void printDetails(double outstanding) {
+		System.out.println("name: " + name);
+		  System.out.println("amount: " + outstanding);
+	}
+	private double printOwings(Enumeration elements, double outstanding) {
+		while (elements.hasMoreElements()) {
+		    Order each = (Order) elements.nextElement();
+		    outstanding += each.getAmount();
+		  }
+		return outstanding;
+	}
+	private void printBanner() {
+		// print banner
+		  System.out.println ("*****************************");
+		  System.out.println ("****** Customer totals ******");
+		  System.out.println ("*****************************");
 	}
 	
 	class Order implements Enumeration {
