48115c3989e8acb3e299f44027aeb9b61dc3c703
Andrew Sutiono <andrewjogja1@hotmail.com>
2025-02-13 20:00:36 +0700

    memperbaiki dengan membuat class gammaCalculator dengan method object

--- src/exercise/MethodObject.java
+++ src/exercise/MethodObject.java
@@ -8,17 +8,38 @@ public class MethodObject {
 		}
 		// TODO: replace this gamma method with method object (make a class for the gamma logic)  
 		public int gamma(int inputVal, int quantity, int yearToDate) {
-			    int importantValue1 = (inputVal * quantity) + delta();
-			    int importantValue2 = (inputVal * yearToDate) + 100;
-			    if ((yearToDate - importantValue1) > 100) {
-			      importantValue2 -= 20;
-			    }
-			    int importantValue3 = importantValue2 * 7;
-			    // and so on�
-			    return importantValue3 - 2 * importantValue1;
-			  }
+			    return new GammaCalculator(this, inputVal, quantity, yearToDate).calculate();
+
+		}
 
 	}
+	
+	class GammaCalculator{
+		private Account account;
+		int inputVal;
+		int quantity;
+		int yearToDate;
+		int importantValue1;
+	    int importantValue2;
+		public GammaCalculator(Account account, int inputVal, int quantity, int yearToDate) {
+			this.account = account;
+			this.inputVal = inputVal;
+			this.quantity = quantity;
+			this.yearToDate = yearToDate;
+		}
+	    public int calculate() {
+	    	int importantValue1 = (inputVal * quantity) + account.delta();
+		    int importantValue2 = (inputVal * yearToDate) + 100;
+		    if ((yearToDate - importantValue1) > 100) {
+		      importantValue2 -= 20;
+		    }
+		    int importantValue3 = importantValue2 * 7;
+		    // and so on�
+		    return importantValue3 - 2 * importantValue1;
+	    }
+	    
+	}
+	
 	private void test() {
 		Account account = new Account();
 		System.out.println(account.gamma(15,10,2010));
