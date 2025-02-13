adb6eeae51fc38865d8803e09344905663d563d4
Andrew Sutiono <andrewjogja1@hotmail.com>
2025-02-13 19:49:52 +0700

    extract the JobItem class to PartsItem and LaborItem

--- src/exercise/ExtractSubClass.java
+++ src/exercise/ExtractSubClass.java
@@ -1,17 +1,12 @@
 ﻿package exercise;
 public class ExtractSubClass {
 // todo: extract subclass PartsItem & LaborItem from JobItem
-	class JobItem {
+	abstract class JobItem {
 	  private int quantity;
-	  private int unitPrice;
-	  private Employee employee;
-	  private boolean isLabor;
 
-	  public JobItem(int quantity, int unitPrice, boolean isLabor, Employee employee) {
+
+	  public JobItem(int quantity) {
 		this.quantity = quantity;
-		this.unitPrice = unitPrice;
-		this.isLabor = isLabor;
-		this.employee = employee;
 	  }
 	  public int getTotalPrice() {
 		return quantity * getUnitPrice();
@@ -19,12 +14,38 @@ public class ExtractSubClass {
 	  public int getQuantity() {
 		return quantity;
 	  }
-	  public int getUnitPrice() {
-		return (isLabor) ? employee.getRate() : unitPrice;
-	  }
-	  public Employee getEmployee() {
-		return employee;
-	  }
+	  public abstract int getUnitPrice();
+	}
+	class PartsItem extends JobItem{
+		private int unitPrice;
+		public PartsItem(int quantity, int unitPrice) {
+			super(quantity);
+			this.unitPrice = unitPrice;
+		}
+
+		public int getUnitPrice() {
+			return unitPrice;
+		}
+
+		public void setUnitPrice(int unitPrice) {
+			this.unitPrice = unitPrice;
+		}
+	}
+	class LaborItem extends JobItem{
+		private Employee employee;
+		public LaborItem(int quantity, Employee employee) {
+			super(quantity);
+			this.employee = employee;
+		}
+		public Employee getEmployee() {
+			return employee;
+		}
+		public void setEmployee(Employee employee) {
+			this.employee = employee;
+		}
+		public int getUnitPrice() {
+			return employee.getRate();
+		}
 	}
 
 	class Employee {
@@ -38,8 +59,8 @@ public class ExtractSubClass {
 	}
 	public void action() {
 		Employee kent = new Employee(50);
-		JobItem j1 = new JobItem(5, 0, true, kent);
-		JobItem j2 = new JobItem(15, 10, false, null);
+		JobItem j1 = new LaborItem(5, kent);
+		JobItem j2 = new PartsItem(15, 10);
 		int total = j1.getTotalPrice() + j2.getTotalPrice();
 		System.out.println(total);
 	}
